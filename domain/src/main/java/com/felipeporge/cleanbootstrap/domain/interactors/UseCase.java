ce6962e1db9e3a4edb9188a0841c78395ecd31b2
felipefpx <felipefpx@gmail.com>
2017-08-02 10:19:04 -0300

    Turning callback private.

--- domain/src/main/java/com/felipeporge/cleanbootstrap/domain/interactors/UseCase.java
+++ domain/src/main/java/com/felipeporge/cleanbootstrap/domain/interactors/UseCase.java
@@ -86,11 +86,4 @@ public abstract class UseCase<REPO, PARAM, RESULT> implements Runnable {
         }
     }
 
-    /**
-     * Gets use case callback.
-     * @return  Use case callback.
-     */
-    public UseCaseCallback<RESULT> getCallback(){
-        return mCallback;
-    }
 }
